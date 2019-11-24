c98101e9374e21ae1494f4e32d6cc135fc08df4f
Hidayat Riva'i <stu.dent.palawija@gmail.com>
2019-11-24 09:40:53 +0700

    Ruby Symbol

---
+++ symbol.rb
@@ -0,0 +1,10 @@
+provinsi = {
+	:jabar => "Jawa Barat",
+	:jateng => "Jawa Tengah",
+	:jatim => "Jawa Timur",
+}
+
+puts "Provinsi : #{provinsi}"
+puts "Jumlah : #{provinsi.length}"
+puts "Kelas : #{provinsi.class()}"
+puts "jatim : #{provinsi[:jatim]}"
